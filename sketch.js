67f626a3efe9171ad3e621a59b05dbd25407740f
Ricardo Antonio López Castillo <rychy126@gmail.com>
2021-06-03 00:53:27 -0600

    prueba 1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  print("hola mundo");
 }
 
 function draw() {
-  background(220,0,200);
+  background(130,0,250);
+  rectMode(CENTER);
+  fill(150,100,20,95);
+  stroke(255);
+  strokeWeight(5);
+  rect(200,200,100, 200, 20);
+  //line(15,40,300,200, 10,100);
 }
